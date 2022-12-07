edd589715c1c6e5afcd06fa31b12102c6f0f46b1
Vasily <invweb@gmail.com>
2022-12-07 10:48:40 +0300

    deleted unused file, added new data class(Town)

--- app/src/main/java/com/zx_tole/openstreetmap/tech/Coordinate.kt
+++
@@ -1,7 +0,0 @@
-package com.zx_tole.openstreetmap.tech
-
-interface Coordinate {
-    fun setX()
-    fun detY()
-    fun setName()
-}
---
+++ app/src/main/java/com/zx_tole/openstreetmap/tech/Town.kt
@@ -0,0 +1,3 @@
+package com.zx_tole.openstreetmap.tech
+
+data class Town(val x: Double, val y: Double, val name: String)
